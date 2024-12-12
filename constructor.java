daac25178c3c816adfccda4c90a84bb978769529
anushastefyj <24hr5a3001@mtieat.org>
2024-12-12 10:46:16 +0530

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,21 @@
+class constructor
+{
+    int c;
+    String b;
+    constructor()
+    {
+        this.c=435;
+        this.b="heloo";
+    }
+    void display()
+        {
+            System.out.println(b+" "+c);
+        }
+        public static void main(String args[])
+        {
+            constructor s=new constructor();
+            s.display();
+        }
+ }
+
+
