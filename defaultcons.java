efd108aac8fc1b0e827b1f1f8e26d4792c99f00f
anushastefyj <24hr5a3001@mtieat.org>
2024-12-12 10:49:36 +0530

    Add files via upload

---
+++ defaultcons.java
@@ -0,0 +1,21 @@
+class defaultcons
+{
+    int c;
+    String b;
+    defaultcons()
+    {
+        this.c=435;
+        this.b="heloo";
+    }
+    void display()
+        {
+            System.out.println(b+" "+c);
+        }
+        public static void main(String args[])
+        {
+            defaultcons s=new defaultcons();
+            s.display();
+        }
+ }
+
+
